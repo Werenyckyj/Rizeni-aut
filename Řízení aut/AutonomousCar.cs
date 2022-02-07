e3f70b8dae4b6dbe086ea23fb03928545138bf5e
werenyckyj.vo.2019 <werenyckyj.vo.2019@UC313PC09>
2022-02-07 14:26:48 +0100

    Přidejte soubory projektu.

---
+++ Řízení aut/AutonomousCar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Řízení_aut
+{
+    public class AutonomousCar
+    {
+        public double speed = 90;
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+        public string Condition { get; set; }
+        public string Route { get; set; }
+        public AutonomousCar()
+        {
+            RouteGenerator();
+        }
+        public void RouteGenerator()
+        {
+            string s = "0";
+            Random r = new Random();
+            while (s.Length < 10)
+            {
+                s += r.Next(0, 2);
+            }
+            Route = s;
+            PositionX = r.Next(0,100);
+            PositionY = r.Next(0,100);
+        }
+    }
+}
---
+++ Řízení aut/WeatherStation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Řízení_aut
+{
+    public class WeatherStation
+    {
+        public Random r = new Random();
+        public int Weather = r.Next(0, 4);
+    }
+}
